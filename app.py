bbc2ef564fc6d9bf91aa2e324e65ed739347c407
Aryunaparis <118813225+Aryunaparis@users.noreply.github.com>
2023-03-22 00:17:02 +0100

    Delete app.py

--- app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-app = Flask("My Flask Application")
-@app.route("/")
-def hello():
-    return "<h1>My First Hello World!</h1>"
-if __name__=="__main__":
-    app.run(debug=True) 
-    # When no port is specified, starts at default port 5000
